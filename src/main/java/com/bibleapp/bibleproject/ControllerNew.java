5c8060923099153fc72164fcfa31038e8c8667d1
SamAshray1 <sam.ashray1@gmail.com>
2025-01-19 22:29:04 +0530

    implmented jwt auth for /add-verse

--- src/main/java/com/bibleapp/bibleproject/ControllerNew.java
+++ src/main/java/com/bibleapp/bibleproject/ControllerNew.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +32,7 @@ public class ControllerNew {
 
 	
 	@PostMapping(path = "/add-verse")
+	@PreAuthorize("hasAuthority('ROLE_USER')")
 	public ResponseEntity<String> addVerse(@RequestHeader("Authorization") String authorizationHeader, @RequestBody Reference reference) {
         
 		// Check if the authorization header is present
--- src/main/java/com/bibleapp/bibleproject/controller/UserController.java
+++ src/main/java/com/bibleapp/bibleproject/controller/UserController.java
@@ -1,16 +1,22 @@
 package com.bibleapp.bibleproject.controller;
 
+import com.bibleapp.bibleproject.Reference;
 import com.bibleapp.bibleproject.entity.AuthRequest;
 import com.bibleapp.bibleproject.entity.UserInfo;
 import com.bibleapp.bibleproject.service.JwtService;
 import com.bibleapp.bibleproject.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,6 +30,8 @@ public class UserController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    private List<Reference> referenceList = new ArrayList<>();
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -58,5 +66,34 @@ public class UserController {
         } else {
             throw new UsernameNotFoundException("Invalid user request!");
         }
+    }
+
+    private boolean isExisting(Reference reference) {
+        boolean foundRef = false;
+        for (Reference item : referenceList) {
+            if ((item.getBook().equals(reference.getBook())) &&
+                    (item.getChapter().equals(reference.getChapter())) &&
+                    (item.getVerse().equals(reference.getVerse()))) {
+                System.out.print("dupe");
+                foundRef = true;
+                break;
+            }
+        }
+
+        return foundRef;
+    }
+
+    @PostMapping(path = "/user/add-verse-jwt")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public ResponseEntity<String> addVerse(@RequestBody Reference reference) {
+
+        if(isExisting(reference)) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("{\"message\": \"Reference already exists.\"}");
+        }else {
+            referenceList.add(reference);
+
+            return ResponseEntity.ok("{\"message\": \" Reference added successfully.\" }");
+        }
+
     }
 }
